a9682f649ed28d365294a674bb6090face1bf9c8
Kishor6941 <phawadeks9049@gmail.com>
2025-12-06 20:47:26 +0530

    read json file data

--- controllers/workerController.js
+++ controllers/workerController.js
@@ -1,5 +1,12 @@
 
 import {Worker} from 'worker_threads';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const data = JSON.parse(fs.readFileSync(path.join(__dirname, '../data.json'), 'utf-8'));
 export const workerThreadTest = (req, res) => {
     const worker = new Worker('C:\\Users\\phawa\\Documents\\express\\controllers\\worker-thread.js')
     let count = 0;
@@ -13,6 +20,7 @@ export const workerThreadTest = (req, res) => {
 }
 
 export const workerDataTest = (req, res) => {
+    console.log(data);
     console.log("Worker Data Test");
     res.send("Worker Data Test");
 }
